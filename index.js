0f965f5eea23a615dbd222362a06ee1696f3fddc
Ville Nupponen <aikain@paivola.fi>
2016-03-08 22:58:44 +0200

    Add index.html, index.css, index.js

---
+++ index.js
@@ -0,0 +1,57 @@
+var zoomLvl = 1;
+var KOKO = 256;
+
+$(function() {
+    $("#rainbow").draggable();
+
+    $('#rainbow').mousewheel(function(event, delta) {
+        var vanhaX = $("#rainbow").offset().left;
+        var vanhaY = $("#rainbow").offset().top;
+        var x = event.pageX - $("#rainbow").offset().left;
+        var y = event.pageY - $("#rainbow").offset().top;
+        var levKor = KOKO * Math.pow(2,(zoomLvl-1));
+        var sijx = x * 100 / levKor;
+        var sijy = y * 100 / levKor;
+        if (0 < zoomLvl + delta && zoomLvl + delta <= 9) {
+            zoomLvl += delta;
+            $("#rainbow").css("width", KOKO * Math.pow(2,(zoomLvl-1)));
+            $("#rainbow").css("height", KOKO * Math.pow(2,(zoomLvl-1)));
+            var ulevKor = KOKO * Math.pow(2,(zoomLvl-1));
+            var usijx = ulevKor/100 * sijx;
+            var usijy = ulevKor/100 * sijy;
+            $("#rainbow").css("top", vanhaY - (usijy - y));
+            $("#rainbow").css("left", vanhaX - (usijx - x));
+        }
+        event.stopPropagation();
+        event.preventDefault();
+    });
+
+    $("#rainbow").onPositionChanged(function(loc){ <!-- loc.top loc.left -->
+        for ( topk = loc.top > 0 ?loc.top : 0; topk < $(window).height(); topk = topk + KOKO) {
+            for ( left = loc.left > 0 ?loc.left : 0; left < $(window).width(); left = left + KOKO) {
+                var monesl = Math.floor(loc.left / KOKO * -1);
+                var monesk = Math.floor(loc.top / KOKO * -1);
+                console.log("Moneskol " + monesl);
+                console.log("Moneskok " + monesk);
+                console.log(loc);
+                console.log("left " + left + " top " + topk );
+                console.log(zoomLvl);
+                var zLvl = zoomLvl-1;
+                if ($("#img_"+zLvl +"_"+monesl+"_"+monesk).length) {
+                    console.log("Kuva olemassa, ei turhaan luoda uutta samalaista!");
+                } else {
+                    console.log("Luodaan kuva");
+                    var img = $('<img>')
+                    img.attr("id", "img_"+zLvl+"_"+monesl+"_"+monesk);
+                    img.addClass("img_" + zLvl);
+                    img.width(256);
+                    img.height(256);
+                    img.attr("src", "kuvat\\"+zLvl+"\\"+zLvl+"_"+monesl+"_"+monesk+".jpg");
+                    img.css("left", monesl * KOKO);
+                    img.css("top", monesk * KOKO);
+                    $("#rainbow").append(img);
+                }
+            }
+        }
+    });
+});
